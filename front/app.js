42b770b8e86d1e8fa0c0035d4d8d54e99f9abad4
Adam Borowski <adam@adamboro.com>
2016-08-21 16:34:54 -0400

    front - display list of reminders

--- front/app.js
+++ front/app.js
@@ -1,5 +1,14 @@
 const $ = require('jquery')
 
-$(document).ready(function(){
-  console.log('hello')
+$(document).ready(function () {
+  const $list = $('#list')
+
+  $.get('/list', (data) => {
+    for (var group in data) {
+      group = data[group]
+      let reminderEl = document.createElement('div')
+      reminderEl.innerHTML = `${group.summary} (${group.events.length})`
+      $list.append(reminderEl)
+    }
+  })
 })
