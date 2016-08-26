68e5fac31748077531bf29dc6d7518fdf383d1df
Adam Borowski <adam@adamboro.com>
2016-08-26 13:23:04 -0400

    updating event summaries - front

--- front/js/components/list-elem.jsx
+++ front/js/components/list-elem.jsx
@@ -3,10 +3,18 @@ import React from 'react'
 import utils from './../utils'
 
 export default class ListElem extends React.Component {
-  constructor () {
-    super()
+  constructor (props) {
+    super(props)
+
     // docs: "We recommend that you bind your event handlers in the constructor so they are only bound once for every instance:"
     this.removeReminder = this.removeReminder.bind(this)
+    this.handleNameChange = this.handleNameChange.bind(this)
+    this.handleNameInputBlur = this.handleNameInputBlur.bind(this)
+
+    this.state = {
+      name: this.props.groupItem.summary,
+      changed: false
+    }
   }
   removeReminder () {
     // optimistic update
@@ -15,9 +23,32 @@ export default class ListElem extends React.Component {
       console.log(resp)
     }, (err) => { console.log(err) })
   }
+  handleNameChange (event) {
+    this.setState({
+      name: event.target.value,
+      changed: true
+    })
+  }
+  handleNameInputBlur (event) {
+    if (this.state.changed) {
+      console.log(event.target.value, this.props.spcId)
+      this.setState({
+        changed: false
+      })
+      utils.ajax({
+        method: 'POST',
+        url: `/api/update/${this.props.spcId}`,
+        data: {eventSummary: event.target.value}
+      }, (resp) => {
+        console.log(resp)
+      }, (err) => { console.log(err) })
+    }
+  }
   render () {
     return <div>
-      <span>{this.props.groupItem.summary} ({this.props.groupItem.events.length})</span>
+      <span className="event-name">
+        <input type="text" ref="eventNameInput" value={this.state.name} onChange={this.handleNameChange} onBlur={this.handleNameInputBlur} />
+      </span>
       <button className="btn-small btn-delete" onClick={this.removeReminder}>delete</button>
     </div>
   }
